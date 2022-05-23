8bdb8d04e21ef12b9e97bc557b9d56fb3f0fe83e
OsmarMendes <99884631+OsmarMendes@users.noreply.github.com>
2022-05-23 11:26:06 -0300

    ajsute

---
+++ atividades/aula 08 - condições(part.1)/exe02.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`você vive no pais chamado ${pais}`)
+if (pais == 'brasil'){
+    console.log('você é brasileiro')
+}else{
+    console.log('você não brasileiro')
+}
+    
---
+++ atividades/aula 08 - condições(part.1)/exer01.js
@@ -0,0 +1,8 @@
+
+var velocidade = 50
+console.log(`a velocidade do carro é de ${velocidade} km/h `)
+if (velocidade > 60){
+    console.log(`sua velocidade foi de ${velocidade} km/h`)
+    console.log('você foi multado. Dirija com cuidado!')
+}
+//console.log('dirija com cuidado!')
