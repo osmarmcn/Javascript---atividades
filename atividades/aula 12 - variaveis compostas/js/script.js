5f40dc2c0a4a102ba92651bb27b873c3273e5b63
OsmarMendes <99884631+OsmarMendes@users.noreply.github.com>
2022-05-31 10:41:19 -0300

    atualizando

---
+++ atividades/aula 12 - variaveis compostas/js/script.js
@@ -0,0 +1,46 @@
+/*let num = [
+    1,
+    2,
+    3,
+    7, 
+    9,
+    12, 
+    15
+]
+console.log(num)
+*/
+
+/*let n = [1,3,6,8,9]
+console.log(n)
+*/
+
+/*let num = [2, 4, 6, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`para apresentar o numero da casa '3' é ${num[3]}`)
+console.log(`a quantidade de elementos dentro do array é ${num.length}`)
+*/
+
+// caso coloque o push abaixo do sort ele vai organizar a ordem primeiro do array e depois vai acrescentar o valor (ficar atento a ordem de como è acrescido os comandos)
+
+/*let valores = [1,2,4,6,3,9,8]
+for(let posicao = 0;posicao <= valores.length; posicao++ ){
+    console.log(`a posição ${posicao} possuindo valores de ${valores[posicao]}`)
+    */
+
+
+    //posicao - é o indice em que vai comceçcar a contagem 
+    // valores.length- é cumpriemnto do array que vai até determinado valor 
+    //posicao++ - é contador 
+    //valores[posicao] - indica a posiÇão dos valores de acordo com a contagem que passa por dentro do array
+    // posso colocar valores.sort()[posicao] para deixar em ordem 
+
+
+let valores = [2,3,5,6,8,4]
+valores.sort()
+for(posicao in valores ){
+    console.log(`a posição ${posicao} possui o valor ${valores[posicao]}`)
+     /*esse modo é o mais simplificado, no qual não necessita de acrescentar contador e declarar com length */
+}
+
