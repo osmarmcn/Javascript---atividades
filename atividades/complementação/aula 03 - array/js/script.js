dd067f65336cce8ba3df22da266fbef9a4136a24
OsmarMendes <99884631+OsmarMendes@users.noreply.github.com>
2022-06-10 18:57:47 -0300

    ajuste

---
+++ atividades/complementação/aula 03 - array/js/script.js
@@ -0,0 +1,24 @@
+/*var lista = ['uva', 'pera', 'morango', 'manga','caju']
+console.log(lista)
+lista.push('maracuja') // acrescenta mais um elemento no final da lista
+console.log(lista)
+lista.pop() // tira o ultimo elemento da lista
+console.log(lista)
+console.log(lista.length)
+console.log(lista.reverse())  // coloca a ordem dos valores ao contrario
+console.log(lista.toString()) // transforma o array em uma string
+console.log(lista.join(" - ")) // deixa o array sem a virgula e coloca o objeto que deseja
+*/
+
+/*var frutas = [{nome:'uva', cor:'roxo'}, {nome:'laranja', cor:'verde'}]
+console.log(frutas)
+console.log(frutas[0].nome)
+console.log(frutas[1].cor)
+console.log(frutas[1].nome, frutas[1].cor)
+*/
+
+var c = 0
+if(c <=100){
+    console.log(c)
+    c = c+1
+}
